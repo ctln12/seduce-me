da628168fab81771078cc3d8337fc6e4934502b5
Cataline Baert <snuf_kin@yahoo.com>
2019-08-20 12:18:19 +0200

    add new column to questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_152739) do
+ActiveRecord::Schema.define(version: 2019_08_20_101505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_08_19_152739) do
     t.bigint "story_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "number"
     t.index ["story_id"], name: "index_questions_on_story_id"
   end
 
