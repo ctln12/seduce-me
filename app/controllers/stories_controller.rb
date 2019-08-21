6520004080313ad1a9337612c31d84ab26991952
Cataline Baert <snuf_kin@yahoo.com>
2019-08-21 15:43:53 +0200

    remove skip before action and add body to method gameover

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,11 +1,12 @@
 class StoriesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :gameover, :win]
-
+  # skip_before_action :authenticate_user!, only: [:index, :gameover, :win]
   def index
     @stories = Story.all
   end
 
   def gameover
+    @story = Story.find(params[:id])
+    @questions = @story.questions
   end
 
   def win
