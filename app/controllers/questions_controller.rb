0a76d81d0cb2345c6b1e0540ac0f3127385865ab
Cataline Baert <snuf_kin@yahoo.com>
2019-08-21 15:43:16 +0200

    remove skip before action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :show
+  # skip_before_action :authenticate_user!, only: :show
   def show
     @story = Story.find(params[:story_id])
     @question = Question.find(params[:id])
