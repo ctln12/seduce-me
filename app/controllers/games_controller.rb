9bef681cb7815ef5840a0a36c28bbd387f928977
Cataline Baert <snuf_kin@yahoo.com>
2019-08-21 19:36:30 +0200

    implement method create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,9 @@
 class GamesController < ApplicationController
+  def create
+    @story = Story.find(params[:story_id])
+    @questions = @story.questions
+    @game = Game.new(user: current_user, story: @story)
+    @game.save
+    redirect_to "/stories/#{@story.id}/questions/#{@questions[0].id}"
+  end
 end
