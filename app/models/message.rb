9bc9bb8e3a0bbfdaf960e0961a60690da2c2aed7
James Hart <jhart2083@gmail.com>
2019-08-21 12:46:39 +0200

    create from_seducer? method in message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :chat
   validates :content, presence: true, allow_blank: false
+
+  def from_seducer?(some_user)
+    chat.game.user == some_user
+  end
 end
