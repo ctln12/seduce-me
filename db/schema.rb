21e3d33bc8880c2f06d56e6930ac316ea683cd3b
Cataline Baert <snuf_kin@yahoo.com>
2019-08-22 11:35:16 +0200

    remove win field from games table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_134054) do
+ActiveRecord::Schema.define(version: 2019_08_22_093344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2019_08_20_134054) do
 
   create_table "games", force: :cascade do |t|
     t.string "user_responses"
-    t.boolean "win"
     t.bigint "user_id"
     t.bigint "story_id"
     t.datetime "created_at", null: false
