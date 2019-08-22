161118f0eb7f120422bbfea70ea895a8f71ead77
Cataline Baert <snuf_kin@yahoo.com>
2019-08-22 12:00:17 +0200

    add new variable to save game score while playing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,8 @@ class QuestionsController < ApplicationController
   def show
     @story = Story.find(params[:story_id])
     @question = Question.find(params[:id])
+    @game = Game.where(user: current_user, story: @story).first
+    @game.score = (@question.number - 1)
+    @game.save
   end
 end
