d0e34ef457d46633adc9145edfca4a984f2f980d
James Hart <jhart2083@gmail.com>
2019-08-26 11:26:06 +0200

    Only current user can see dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  before_action :user_is_current_user
   def show
+    @user = User.find(params[:id])
+  end
 
+
+  private
+
+  def user_is_current_user
+    if current_user != User.find(params[:id])
+      redirect_to root_path, notice: "Sorry, but you are only allowed to view your own profile page."
+    end
   end
 end
