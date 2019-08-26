e74cd2dc92b7b4766dcbd365396d24c2bcc39f13
James Hart <jhart2083@gmail.com>
2019-08-26 11:26:57 +0200

    Update notice for when trying to access another persons dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def user_is_current_user
     if current_user != User.find(params[:id])
-      redirect_to root_path, notice: "Sorry, but you are only allowed to view your own profile page."
+      redirect_to root_path, notice: "Sorry, but you are only allowed to view your own dashboard."
     end
   end
 end
