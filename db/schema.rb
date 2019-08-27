0ef5248cba78fb4eca3137248f062599023c9b67
Cataline Baert <snuf_kin@yahoo.com>
2019-08-27 14:02:14 +0200

    run migration: modify schema

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2019_08_27_113633) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "greetings"
     t.index ["user_id"], name: "index_stories_on_user_id"
   end
 
@@ -76,7 +75,6 @@ ActiveRecord::Schema.define(version: 2019_08_27_113633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
