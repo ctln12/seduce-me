6530a87713c067f77d616c3e5da9c69e2e8859da
Cataline Baert <snuf_kin@yahoo.com>
2019-08-29 12:23:04 +0200

    add private method to gameover and win

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -15,6 +15,8 @@ class StoriesController < ApplicationController
     @story = Story.find(params[:id])
     @questions = @story.questions
     @game = Game.where(user: current_user, story: @story).first
+    current_user.ranking = compute_ranking
+    current_user.save
   end
 
   def win
@@ -23,6 +25,8 @@ class StoriesController < ApplicationController
     @game = Game.where(user: current_user, story: @story).first
     @game.score = @questions.count
     @game.save
+    current_user.ranking = compute_ranking
+    current_user.save
   end
 
   private
@@ -30,6 +34,6 @@ class StoriesController < ApplicationController
   def compute_ranking
     all_games = Game.where(user: current_user)
     games_won = Game.where(user: current_user, score: 3)
-    (games_won.count.fdiv(all_games.count) * 100).round
+    (games_won.count.fdiv(all_games.count) * 100).to_i
   end
 end
